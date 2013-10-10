0ad6391ae5dc651c49374357ee2c1d2a27a23711
Matthias Becker <matthias.becker@se-co.de>
2013-10-10 14:30:46 +0200

    hghg
    fgh

---
+++ serp/src/main/java/de/seco/serp/App.java
@@ -0,0 +1,13 @@
+package de.seco.serp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
