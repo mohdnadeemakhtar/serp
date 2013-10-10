208ae218f2b762db48aad2540ab972a3ef96b74f
Matthias Becker <matthias.becker@se-co.de>
2013-10-10 16:30:31 +0200

    renamed App

---
+++ src/main/java/de/seco/serp/Application.java
@@ -0,0 +1,16 @@
+package de.seco.serp;
+
+/**
+ * Serp
+ *
+ */
+public class Application 
+{
+	/**
+	 * @param args
+	 */
+    public static void main( String[] args )
+    {
+        System.out.println( "SERP" );
+    }
+}
