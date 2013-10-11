a60cc9da5e75e8e597f956f3c573ca94c5b8553b
ahanisch <ahanisch@192.168.0.93>
2013-10-11 12:55:12 +0200

    graph db path

--- src/main/java/de/seco/serp/DataSource.java
+++ src/main/java/de/seco/serp/DataSource.java
@@ -6,7 +6,7 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 public class DataSource {
 
 	private static GraphDatabaseService graphDb;
-	public final static String GRAPH_DB_PATH = "C:/SERP-graphdb";
+	public final static String GRAPH_DB_PATH = "/SERP-graphdb";
 	
 	public static void initGraphDB () {
 		System.out.println("init graph database " + GRAPH_DB_PATH);
