5cea732d9de558952cdeece83c3f68cf518c5b18
ahanisch <ahanisch@192.168.0.93>
2013-10-11 11:13:30 +0200

    ...

---
+++ src/main/java/de/seco/serp/MainServletContextListener.java
@@ -0,0 +1,16 @@
+package de.seco.serp;
+
+import javax.servlet.ServletContext; 
+import javax.servlet.ServletContextEvent; 
+import javax.servlet.ServletContextListener;
+	
+public class MainServletContextListener implements ServletContextListener {
+
+	public void contextInitialized(ServletContextEvent sce) {
+		System.out.println ("SERP is starting...");
+	}
+	
+	public void contextDestroyed(ServletContextEvent sce) {
+		System.out.println ("SERP is stopped.");
+	}
+}
