4d0b595da3183e4a773e89c2c3f577dc2e278231
ahanisch <ahanisch@192.168.0.93>
2013-10-11 12:14:32 +0200

    init graphDb skeleton

--- src/main/java/de/seco/serp/DataSource.java
+++ src/main/java/de/seco/serp/DataSource.java
@@ -1,10 +1,23 @@
 package de.seco.serp;
 
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 public class DataSource {
 
 	private static GraphDatabaseService graphDb;
+	public final static String GRAPH_DB_PATH = "";
+	
+	public static void initGraphDB () {
+		System.out.println("init graph database (not implemented)");
+		//graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( GRAPH_DB_PATH );
+		//registerShutdownHook( graphDb );
+	}
+	
+	public static void closeGraphDB () {
+		System.out.println("close graph database (not implemented)");
+		//graphDb.shutdown();
+	}
 	
 	public static GraphDatabaseService getGraphDB () {
 		return graphDb;
--- src/main/java/de/seco/serp/MainServletContextListener.java
+++ src/main/java/de/seco/serp/MainServletContextListener.java
@@ -8,9 +8,12 @@ public class MainServletContextListener implements ServletContextListener {
 
 	public void contextInitialized(ServletContextEvent sce) {
 		System.out.println ("SERP is starting...");
+		
+		DataSource.initGraphDB();
 	}
 	
 	public void contextDestroyed(ServletContextEvent sce) {
 		System.out.println ("SERP is stopped.");
+		DataSource.closeGraphDB();
 	}
 }
