a006bd593a02e9d1e7a2a1bf95f7f6602e57376c
ahanisch <ahanisch@192.168.0.93>
2013-10-11 12:19:43 +0200

    use some graphdb path for testing

--- src/main/java/de/seco/serp/DataSource.java
+++ src/main/java/de/seco/serp/DataSource.java
@@ -6,17 +6,17 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 public class DataSource {
 
 	private static GraphDatabaseService graphDb;
-	public final static String GRAPH_DB_PATH = "";
+	public final static String GRAPH_DB_PATH = "C:/SERP-graphdb";
 	
 	public static void initGraphDB () {
-		System.out.println("init graph database (not implemented)");
-		//graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( GRAPH_DB_PATH );
+		System.out.println("init graph database " + GRAPH_DB_PATH);
+		graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( GRAPH_DB_PATH );
 		//registerShutdownHook( graphDb );
 	}
 	
 	public static void closeGraphDB () {
-		System.out.println("close graph database (not implemented)");
-		//graphDb.shutdown();
+		System.out.println("close graph database");
+		graphDb.shutdown();
 	}
 	
 	public static GraphDatabaseService getGraphDB () {
