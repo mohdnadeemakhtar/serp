f478f358c7051ff3538ac0476971cc5be926b93e
ahanisch <ahanisch@192.168.0.93>
2013-10-16 14:11:02 +0200

    graphDbPath

--- src/main/java/de/seco/serp/MainServletContextListener.java
+++ src/main/java/de/seco/serp/MainServletContextListener.java
@@ -1,5 +1,7 @@
 package de.seco.serp;
 
+import java.io.File;
+
 import javax.servlet.ServletContext; 
 import javax.servlet.ServletContextEvent; 
 import javax.servlet.ServletContextListener;
@@ -10,6 +12,11 @@ public class MainServletContextListener implements ServletContextListener {
 
 	public void contextInitialized(ServletContextEvent sce) {
 		System.out.println ("SERP is starting...");
+		
+		String possibleGraphDbPath = (new File(sce.getServletContext().getRealPath("/"))).toPath().getRoot().toString() + "SERP-graphdb";
+		
+		System.out.println ("possibleGraphDbPath: " + possibleGraphDbPath);
+		//.getRealPath("/");
 		DataSource.initGraphDb();
 		if (!SerpDbSchemaDefinition.getInstance().initSchema()) {
 			System.out.println("could not init schema");
